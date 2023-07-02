749b0bb0442a0cef2130350dff92e7127c3a9bbe
JYOTHI SAI KIRAN NVN <jyothisaikiran11@gmail.com>
2023-07-02 15:15:52 +0530

    done

---
+++ login.js
@@ -0,0 +1,107 @@
+let container = document.getElementById('container')
+
+toggle = () => {
+	container.classList.toggle('sign-in')
+	container.classList.toggle('sign-up')
+}
+
+setTimeout(() => {
+	container.classList.add('sign-in')
+}, 200)
+
+/*----------------------------------------------------------------------------------------------*/
+
+const form = document.getElementById('form');
+const form1 = document.getElementById('form');
+const username = document.getElementById('username');
+const username1 = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password1 = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form,form1.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+	const username1Value = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+	const password1Value = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
+    }
+
+	if(username1Value === '') {
+        setError(username1, 'Username is required');
+    } else {
+        setSuccess(username1);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
+    }
+
+	if(password1Value === '') {
+        setError(password1, 'Password is required');
+    } else if (password1Value.length < 8 ) {
+        setError(password1, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
+    }
+
+    if(password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
+    }
+
+};
+function clickinner() {
+    window.location.href = 'home.html';
+    }
